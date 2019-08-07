7c3fb7619d6b018cc5803805ad1564c803ca7245
Victor Martinez <vcrmartinez@gmail.com>
2019-08-06 22:55:39 -0300

    Change sqlite db name

--- todo/settings.py
+++ todo/settings.py
@@ -2,7 +2,7 @@ from decouple import config
 
 
 DEBUG = config("DEBUG", cast=bool, default=False)
-DATABASE_URI = config("DATABASE_URI", "sqlite:///app.db")
+DATABASE_URI = config("DATABASE_URI", "sqlite:///db.sqlite3")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATION",
         cast=bool, default=False)
