21822822b1c5ff27423d6da31236616f0cd5f971
Victor Martinez <vcrmartinez@gmail.com>
2019-08-06 22:39:09 -0300

    Set up sqlalchemy

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 
+from todo import settings
 from todo.api.schema import schema
+from todo.extensions import db
 
 
 def create_app(settings_override=None):
     app = Flask(__name__)
-    if settings_override:
-        app.config.update(settings_override)
 
+    config(app, settings_override)
     routes(app)
+    extensions(app)
     return app
 
 
-def routes(app):
+def config(app, settings_override):
+    app.config["DEBUG"] = settings.DEBUG
 
+    if settings_override:
+        app.config.update(settings_override)
+
+
+def routes(app):
     @app.route("/")
     def hello():
         return "Go to /graphql"
@@ -25,3 +33,7 @@ def routes(app):
                                     graphiql=True)
 
     app.add_url_rule('/graphql', view_func=view_func)
+
+
+def extensions(app):
+    db.init_app(app)
