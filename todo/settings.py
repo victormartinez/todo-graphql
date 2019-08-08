18e2f43fb3e984f1b669e97aca6bc5e4a957e3cb
Victor Martinez <vcrmartinez@gmail.com>
2019-08-07 23:14:07 -0300

    Fix settings to avoid default values

--- todo/settings.py
+++ todo/settings.py
@@ -1,7 +1,6 @@
 from decouple import config
 
 
-DEBUG = config("DEBUG", cast=bool, default=False)
-DATABASE_URI = config("DATABASE_URI", "postgres+psycopg2://postgres:postgres@localhost:5432/todo_db")
-
-SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATION", cast=bool, default=False)
+DEBUG = config("DEBUG", cast=bool)
+DATABASE_URI = config("DATABASE_URI")
+SQLALCHEMY_TRACK_MODIFICATIONS = config("SQLALCHEMY_TRACK_MODIFICATIONS", cast=bool)
