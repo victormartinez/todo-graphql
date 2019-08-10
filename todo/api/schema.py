50d8cd334822a65b2f6c1262211ef316b75979b8
Victor Martinez <vcrmartinez@gmail.com>
2019-08-09 23:31:45 -0300

    Split schema/mutation into separate modules

--- todo/api/schema.py
+++ todo/api/schema.py
@@ -1,15 +1,7 @@
 import graphene
-from graphene_sqlalchemy import SQLAlchemyConnectionField
 
-from todo.api.types import Task
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(description="A typical hello world")
-    tasks = SQLAlchemyConnectionField(Task)
-
-    def resolve_hello(self, info):
-        return "World"
+from todo.api.query import Query
+from todo.api.mutation import Mutation
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
